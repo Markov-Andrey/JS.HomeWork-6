cbad3d75101f7759c3d3c013bfba6cf5a9ae82b2
Markov-Andrey <andremarkov@icloud.com>
2022-10-12 17:33:23 +0300

    start work

---
+++ script/script6.js
@@ -0,0 +1,18 @@
+//Скрипт позволяющий переключать стиль элемента при нажатии на чек-бокс "Задание проверено"
+function approved(i) {
+let chbox = document.getElementById(`div${i}`);
+let chAppr = document.getElementById(`checkAproved${i}`);
+chbox.classList.toggle('approved');
+chAppr.classList.toggle('checkAproved');
+}
+
+//Task 1
+//Task 2
+//Task 3
+//Task 4
+//Task 5
+//Task 6
+//Task 7
+//Task 8
+//Task 9
+//Task 10
