94f782c5390ef805ecc5be2cff72bfc7d8897fe9
Markov-Andrey <andremarkov@icloud.com>
2022-10-13 08:27:08 +0300

    task 4 and 5 complete

--- script/script6.js
+++ script/script6.js
@@ -40,7 +40,37 @@ function task3() {
 	document.write ('<b>Метод slice:</b>' + str.slice(2,-12) + ' ' + str.slice(6,-1) + '<br>');
 };
 //Task 4
+function task4() {
+	document.write(`Есть переменная массив arr.<br>
+		Создаем переменную для записи результата.<br>
+		С помощью цикла for от 0 до конца массива arr считаем сумму кубов чисел Math.pow(arr[i], 3).<br>
+		Из результата вычисляем квадратный корень Math.sqrt(result).<br>
+		При отображении ответа дополнительно округляем значение до 2 чисел после запятой методом toFixed(2).
+		<br><br>`)
+	let arr = [4, 2, 5, 19, 13, 0, 10];
+	let result = 0;
+	for (let i = 0; i < arr.length; i++) {
+		result += Math.pow(arr[i], 3);
+	}
+	result = Math.sqrt(result);
+	document.write ('<b>Ответ:</b>' + result.toFixed(2) + '<br>');
+};
 //Task 5
+function task5_1(a,b) {
+	let c = Math.abs(a - b);
+	return c;
+};
+function task5_2() {
+	document.write(`Т.к. нас просят сделать 1 действие 2 раза, 
+		в качестве решения создадим функцию.<br>
+		Создаем функцию с 2 аргументами A и B.<br>
+		Создаем переменную С и присваимаем ей модуль разности (A - B) с 
+		помощью объекта Math с методом abs Math.abs(a - b)<br>
+		Вызываем функцию с требуемыми значениями по условию задания.
+		<br><br>`)
+	document.write('<b>Ответ:</b> ' + task5_1(3,5) + '<br>');
+	document.write('<b>Ответ:</b> ' + task5_1(6,1) + '<br>');
+};
 //Task 6
 //Task 7
 //Task 8
